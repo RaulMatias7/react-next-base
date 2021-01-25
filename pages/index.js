ec8ab6fa17dcf473892b616600436a3c5ae212b6
Raúl Matias <63962174+RaulMatias7@users.noreply.github.com>
2021-01-25 23:36:08 +0100

    Codigo da primeira aula da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+//CRIANDO MEUS COMPONENTES
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+
+        <Widget>
+          <Widget.Header>
+              <h1>Bora lá codar</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Teste lari jsdbaids...</p>
+          </Widget.Content>
+        </Widget>
+
+
+        <Widget>
+          <Widget.Content>
+            <h1>Meus Quizes</h1>
+            <p>Teste lari jsdbaids...</p>
+          </Widget.Content>
+        </Widget>
+       <Footer/>
+      </QuizContainer>   
+      <GitHubCorner projectUrl= "https://github.com/RaulMatias7"/> 
+    </QuizBackground>
+  );
 }
